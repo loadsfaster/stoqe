22acb49d6db1478020ad54c6da6b3b4800e0ee79
STOQE <stoqe@stoqe.com>
2019-11-13 17:27:11 +1100

    .

--- explore-salt-and-light/config.js
+++ explore-salt-and-light/config.js
@@ -1,5 +1,5 @@
 var config = {
-    style: 'mapbox://styles/branigan/cjz37rcb003ib1cr3s8rnkt2d',
+    style: 'mapbox://styles/stoqe/ck2vkzzei0trh1cruy6nxv5z9',
     accessToken: 'pk.eyJ1Ijoic3RvcWUiLCJhIjoiY2sydml5YjFpMDVvMTNkbHE3Z21vcmt6diJ9.HEwcTWlpQWl-MmwCQNuWng',
     showMarkers: true,
     alignment: 'left',
@@ -7,13 +7,13 @@ var config = {
     title: '',
     subtitle: '',
     byline: 'Explore Salt + Light (A working demo by STOQE)',
-    footer: 'This is still a work-in-progress but would be a beautiful way to showcase the incredible location of S+L.',
+    footer: 'This StoryMap is a work-in-progress but would be a beautiful way to showcase the incredible location of S+L and really engage visitors with emotion. And we all know emotions sell.',
     chapters: [
         {
             id: 'glacier-np',
             image: 'https://www.stoqe.com/i/sandl/logo.jpg',
             title: '',
-            description: '<strong class="text-lg">Scroll down to explore &darr;</strong>',
+            description: '<strong class="text-lg">Scroll down to Explore &darr;</strong>',
             location: {
                 center: [153.46465, -28.10502],
                 zoom: 3,
@@ -87,9 +87,23 @@ var config = {
             description: '<span class="text-lg">Scroll down to keep exploring &darr;</span>',
             location: {
                 center: [153.27745, -28.18521],
-                zoom: 12,
+                zoom: 10,
                 pitch: 160,
                 bearing: 70
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'agassiz1998',
+            title: 'Only 50mins to Brisbane',
+            image: 'https://www.stoqe.com/i/sandl/brisbane-sm.jpg',
+            description: '<span class="text-lg">Scroll down to keep exploring &darr;</span>',
+            location: {
+                center: [153.02305, -27.47043],
+                zoom: 9,
+                pitch: 180,
+                bearing: 150
             },
             onChapterEnter: [],
             onChapterExit: []
@@ -119,29 +133,15 @@ var config = {
             ]
         },
         {
-            id: 'agassiz1998',
-            title: 'Only 50mins to Brisbane',
-            image: 'https://www.stoqe.com/i/sandl/brisbane-sm.jpg',
-            description: '<span class="text-lg">Scroll down to keep exploring &darr;</span>',
-            location: {
-                center: [153.02305, -27.47043],
-                zoom: 13.51,
-                pitch: 41.00,
-                bearing: 78.33
-            },
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
             id: 'agassiz2015',
-            title: 'Only 50mins from Byron Bay',
-            image: 'https://www.stoqe.com/i/sandl/byron-sm.jpg',
-            description: '<span class="text-lg">Scroll down to keep exploring &darr;</span>',
+            title: 'At Salt + Light Beach House you\'ll always feel at home.',
+            image: 'https://www.stoqe.com/i/sandl/view-sm.jpg',
+            description: '<span class="text-lg"><a href=\"\">Book Now via Airbnb</a> or <a href=\"\">return to our homepage</a></span>',
             location: {
-                center: [-114.15881, 48.93439],
-                zoom: 8.5,
-                pitch: 51.50,
-                bearing: -26.40
+                center: [153.46465, -28.10502],
+                zoom: 12,
+                pitch: 200,
+                bearing: 67
             },
             onChapterEnter: [
                 {
@@ -155,120 +155,6 @@ var config = {
                     opacity: 0
                 }
             ]
-        },
-        {
-            id: 'rainbow1998',
-            title: 'Rainbow Glacier, 1998',
-            image: '',
-            description: 'Rainbow Glacier is in Glacier National Park in the U.S. state of Montana. The glacier is situated immediately to the east of Rainbow Peak at an elevation between 8,500 feet (2,600 m) and 8,000 feet (2,400 m) above sea level. The glacier has visible crevasses in satellite imagery. Rainbow Glacier has shown modest retreat compared to other glaciers in Glacier National Park.',
-            location: {
-                center: [-114.08659, 48.88039],
-                zoom: 13.09,
-                pitch: 50.00,
-                bearing: -53.60
-            },
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'rainbow2015',
-            title: 'Rainbow Glacier, 2015',
-            image: '',
-            description: 'Between 1998 and 2015, Rainbow Glacier lost 17 acres of surface area (about 6%).',
-            location: {
-                center: [-114.08659, 48.88039],
-                zoom: 13.09,
-                pitch: 50.00,
-                bearing: -53.60
-            },
-            onChapterEnter: [
-                {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0.25
-                }
-            ],
-            onChapterExit: [
-                {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0
-                }
-            ]
-        },
-        {
-            id: 'kintla1998',
-            title: 'Kintla Glacier, 1998',
-            image: '',
-            description: 'Kintla Glacier is in Glacier National Park in the U.S. state of Montana. The glacier is situated on a plateau 2 miles (3.2 km) southwest of Kintla Peak at an elevation between 8,700 feet (2,700 m) and 7,700 feet (2,300 m) above sea level. The glacier has numerous crevasses and is actually two glaciers.',
-            location: {
-                center: [-114.18755, 48.92880],
-                zoom: 13.09,
-                pitch: 48.50,
-                bearing: 164.00
-            },
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'kintla2015',
-            title: 'Kintla Glacier, 2015',
-            image: '',
-            description: 'Between 1998 and 2015, Harrison Glacier lost 24 acres of surface area (about 10%).',
-            location: {
-                center: [-114.18755, 48.92880],
-                zoom: 13.09,
-                pitch: 48.50,
-                bearing: 164.00
-            },
-            onChapterEnter: [
-                {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0.25
-                }
-            ],
-            onChapterExit: [
-                {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0
-                }
-            ]
-        },
-        {
-            id: 'sperry1998',
-            title: 'Sperry Glacier, 1998',
-            image: '',
-            description: 'Sperry Glacier is a glacier on the north slopes of Gunsight Mountain west of the Continental Divide in Glacier National Park in the U.S. state of Montana. Although many geologic features of Glacier National Park were formed during the much longer period of glaciation ending over 10,000 years ago, Sperry Glacier — like all the glaciers in the park today — is a product of the recent Little Ice Age, the period of cooler average temperatures starting in about the 13th century and concluding in the mid-19th century.',
-            location: {
-                center: [-113.75672, 48.62433],
-                zoom: 13.68,
-                pitch: 34.50,
-                bearing: 106.40
-            },
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'sperry2015',
-            title: 'Sperry Glacier, 2015',
-            image: '',
-            description: 'Between 1998 and 2015, Harrison Glacier lost 37 acres of surface area (about 16%).',
-            location: {
-                center: [-113.75672, 48.62433],
-                zoom: 13.68,
-                pitch: 34.50,
-                bearing: 106.40
-            },
-            onChapterEnter: [
-                {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0.25
-                }
-            ],
-            onChapterExit: [
-                {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0
-                }
-            ]
-         }
+        }
     ]
 };
